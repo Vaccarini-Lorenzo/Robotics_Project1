ddf45daec686da6375ef20184d861d3b7916c9e3
emiliocenturelli <10576649@polimi.it>
2021-05-13 12:04:51 +0200

    Implemented services to reset odometry at runtime, added launchfile

--- robotics_hw1/src/integrator.cpp
+++ robotics_hw1/src/integrator.cpp
@@ -11,6 +11,9 @@
 #include <dynamic_reconfigure/server.h>
 #include "robotics_hw1/CustomOdom.h"
 #include <robotics_hw1/integrationConfig.h>
+#include <sstream>
+#include "robotics_hw1/SetZeroPose.h"
+#include "robotics_hw1/SetPose.h"
 
 char integration_method; // e => euler; r => runge-kutta
 
@@ -24,36 +27,61 @@ void parameterCallback(robotics_hw1::integrationConfig &config, uint32_t level){
   }
 }
 
+geometry_msgs::Pose parseParam(std::string pose_str, double *th) {
+  geometry_msgs::Pose pose;
+  double theta;
+  std::vector<std::string> v;
+  std::stringstream ss(pose_str);
+
+  while (ss.good()) {
+    std::string substring;
+    getline(ss, substring, ',');
+    v.push_back(substring);
+  }
+
+  pose.position.x = atof(v[0].c_str());
+  pose.position.y = atof(v[1].c_str());
+  theta = atof(v[2].c_str());
+  *th = theta; // DANGEROUS
+  tf2::Quaternion q;
+  q.setRPY(0, 0, theta);
+  pose.orientation = tf2::toMsg(q);
+
+  return pose;
+}
+
+
 class integrator_pub_sub {
 
 private:
   ros::NodeHandle n;
   tf::TransformBroadcaster broadcaster;
   ros::Subscriber sub;
-  ros::Subscriber pose_sub;
   ros::Publisher pub;
   ros::Publisher pub1;
+  ros::ServiceServer service_zero;
+  ros::ServiceServer service_custom;
   geometry_msgs::Pose curr_pose;
   double th;
+  std::string initial_pose_str;
   ros::Time last_time;
   ros::Time current_time;
+
 public:
   integrator_pub_sub() {
     sub = n.subscribe("/wheels_velocity", 1, &integrator_pub_sub::callback, this);
-    pose_sub = n.subscribe("/reset_odom", 1, &integrator_pub_sub::pose_callback, this);
     pub = n.advertise<nav_msgs::Odometry>("/my_odom", 1);
     pub1 = n.advertise<robotics_hw1::CustomOdom>("/custom_odom", 1);
 
-    curr_pose.position.x = 0;
-    curr_pose.position.y = 0;
-    tf2::Quaternion q;
-    q.setRPY(0, 0, 0);
-    curr_pose.orientation = tf2::toMsg(q);
+    n.getParam("/initial_pose", initial_pose_str);
+    curr_pose = parseParam(initial_pose_str, &th);
+    ROS_INFO("Position from static parameter: %.3f %.3f %.3f", curr_pose.position.x, curr_pose.position.y, th);
+
+    service_zero = n.advertiseService("set_zero_pose", &integrator_pub_sub::srvcall_zero, this);
+    service_custom = n.advertiseService("set_pose", &integrator_pub_sub::srvcall_custom, this);
 
     last_time = ros::Time::now();
     current_time = ros::Time::now();
-//  Settato di default su RK tramite dynamic_reconfigure
-//  integration_method = 'r';
   }
 
   void euler(const geometry_msgs::TwistStamped::ConstPtr& v, double dt) {
@@ -116,48 +144,48 @@ public:
     last_time = current_time;
     pub.publish(odom);
     pub1.publish(custom_odom);
-    ROS_INFO("pose:\n\tx= %f\n\ty= %f\n\ttheta= %f", curr_pose.position.x, curr_pose.position.y, th);
+    ROS_INFO("pose:\n\tx= %.3f\n\ty= %.3f\n\ttheta= %.3f", curr_pose.position.x, curr_pose.position.y, th);
   }
 
-  void pose_callback(const geometry_msgs::Pose::ConstPtr& p) {
-    // tf::Transform transform;
-    tf::Quaternion orientation( p->orientation.x,
-                                p->orientation.y,
-                                p->orientation.z,
-                                p->orientation.w);
-    // nav_msgs::Odometry odom;
-    //
-    // current_time = ros::Time::now();
-    curr_pose = *p;
-    this->th = tf::getYaw(orientation);
+  bool srvcall_zero(robotics_hw1::SetZeroPose::Request &req, robotics_hw1::SetZeroPose::Response &res) {
+    curr_pose.position.x = 0;
+    curr_pose.position.y = 0;
+    curr_pose.position.z = 0;
+    curr_pose.orientation = tf::createQuaternionMsgFromYaw(0);
+    th = 0;
+    res.ack = "Odometry reset to 0,0,0: SUCCESS";
+    ROS_INFO("Odometry reset x y th : [%.2f,%.2f,%.2f]", curr_pose.position.x, curr_pose.position.y, th);
+    return true;
+  }
 
-    // odom.header.stamp = current_time;
-    // odom.header.frame_id = "odom";
-    // odom.pose.pose = curr_pose;
-    // odom.child_frame_id = "base_link";
-    //
-    // transform.setOrigin(tf::Vector3(curr_pose.position.x, curr_pose.position.y, 0));
-    // transform.setRotation(orientation);
-    // broadcaster.sendTransform(tf::StampedTransform(transform, current_time, "odom", "base_link"));
-    //
-    // last_time = current_time;
-    // pub.publish(odom);
-    ROS_INFO("Odometry reset triggered");
+  bool srvcall_custom(robotics_hw1::SetPose::Request &req, robotics_hw1::SetPose::Response &res) {
+    curr_pose.position.x = req.x;
+    curr_pose.position.y = req.y;
+    curr_pose.position.z = 0;
+    curr_pose.orientation = tf::createQuaternionMsgFromYaw(req.theta);
+    th = req.theta;
+    std::stringstream ssack;
+    ssack << "Odometry reset to " << req.x << " " << req.y << " " << req.theta << ": SUCCESS";
+    res.ack = ssack.str();
+    ROS_INFO("Odometry reset x y th : [%.2f,%.2f,%.2f]", curr_pose.position.x, curr_pose.position.y, th);
+    return true;
   }
 };
 
 
 int main(int argc, char** argv){
-
   ros::init(argc, argv, "integrator");
-  // dynamic reconfigure
+
+  /* dynamic reconfigure */
   dynamic_reconfigure::Server<robotics_hw1::integrationConfig> server;
   dynamic_reconfigure::Server<robotics_hw1::integrationConfig>::CallbackType parameterFunction;
   parameterFunction = boost::bind(&parameterCallback, _1, _2);
   server.setCallback(parameterFunction);
-  // integrator instance
+
+  /* integrator instance */
   integrator_pub_sub my_integrator;
 
+  ros::Rate loop_rate(0.1); // 1 Hz
   ros::spin();
 
   return 0;
--- robotics_hw1/src/service_client.cpp
+++ robotics_hw1/src/service_client.cpp
@@ -3,82 +3,24 @@
 #include "robotics_hw1/SetPose.h"
 #include <geometry_msgs/Pose.h>
 
-
-/*
-TODO:
-  La classe commentata era la prima versione che avevo provato (incapsulavo client e Publisher
-  nella classe).
-  La versione con tutto nel main continua a non funzionare.
-  Se creo un nodo che fa solo da publisher e pubblico una pose sul topic funziona tutto, integrator
-  la legge e setta correttamente l'odometry, se cerco di fare sia service client che publisher nello
-  stesso nodo per qualche strano motivo la pose non viene pubblicata sul topic, quindi integrator non vede
-  nulla quando viene chiamato il service lato server
-*/
-
-// class ServiceClient {
-// private:
-//   ros::NodeHandle n;
-//   ros::Publisher pub;
-//   ros::ServiceClient client;
-//   robotics_hw1::SetPose srv_custom;
-//   robotics_hw1::SetZeroPose srv_zero;
-//   geometry_msgs::Pose pose;
-// public:
-//   ServiceClient(int argc, char **argv) {
-//     pub = n.advertise<geometry_msgs::Pose>("reset_odom", 1);
-//     switch (argc) {
-//       case 2:
-//         client = n.serviceClient<robotics_hw1::SetZeroPose>("set_zero_pose");
-//         if (client.call(srv_zero)) {
-//           pose = srv_zero.response.pose;
-//         } else {
-//           ROS_ERROR("Failed to call SetZeroPose service");
-//         }
-//         break;
-//
-//       case 4:
-//         client = n.serviceClient<robotics_hw1::SetPose>("set_pose");
-//         srv_custom.request.x = atof(argv[1]);
-//         srv_custom.request.y = atof(argv[2]);
-//         srv_custom.request.theta = atof(argv[3]);
-//         if (client.call(srv_custom)) {
-//           pose = srv_custom.response.pose;
-//           ROS_INFO("RECEIVED POSE: x[%f] y[%f]", pose.position.x, pose.position.y);
-//         } else {
-//           ROS_ERROR("Failed to call SetPose service");
-//         }
-//         break;
-//
-//       default:
-//         ROS_ERROR("Failed to call service:");
-//         ROS_ERROR("Insert arguments to call service");
-//         break;
-//     }
-//   }
-//
-//   void send_pose() {
-//     pub.publish(pose);
-//     ROS_INFO("Client has published");
-//   }
-// };
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "service_client");
   ros::NodeHandle n;
-  ros::Publisher pub = n.advertise<geometry_msgs::Pose>("/reset_odom", 1000);
   ros::ServiceClient client;
   robotics_hw1::SetPose srv_custom;
   robotics_hw1::SetZeroPose srv_zero;
-  geometry_msgs::Pose p;
-  ROS_INFO("argc = %d", argc);
-  if (argc == 1) {return 1;}
+
+  if (argc == 1)
+  {
+    ROS_ERROR("Missed arguments: [set-zero | <x> <y> <theta>]");
+    return 1;
+  }
   if (argc == 2)
   {
     client = n.serviceClient<robotics_hw1::SetZeroPose>("set_zero_pose");
     if (client.call(srv_zero)) {
-      p = srv_zero.response.pose;
-      pub.publish(p);
+      ROS_INFO("%s", srv_zero.response.ack.c_str());
     } else {
       ROS_ERROR("Failed to call SetZeroPose service");
     }
@@ -91,17 +33,12 @@ int main(int argc, char **argv)
     srv_custom.request.y = atof(argv[2]);
     srv_custom.request.theta = atof(argv[3]);
     if (client.call(srv_custom)) {
-      p = srv_custom.response.pose;
-      while (pub.getNumSubscribers() < 1){
-        pub.publish(p);
-      }
+      ROS_INFO("%s", srv_custom.response.ack.c_str());
     } else {
       ROS_ERROR("Failed to call SetPose service");
     }
   }
-
-
-  pub.publish(p);
+  
   ros::spinOnce();
 
   return 0;
